8fcc6793ff1790a9707de1de478528c5f3357fd3
snowbitx <WangXiaoYu_1995@outlook.com>
2025-11-01 19:32:32 +0800

    chore: remove test repo

--- apps/test/app/examples/loader.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { Loader } from '@repo/elements/loader'
-</script>
-
-<template>
-  <div class="flex items-center justify-center p-8">
-    <Loader />
-  </div>
-</template>
