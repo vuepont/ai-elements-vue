ad2697e87bc0d6eb9044d311e3ee709154e48e21
Quan <zhouquan147369@gmail.com>
2025-11-09 02:09:27 +0800

    feat: copy and alias branch/action/response components into message suit and update index exports

--- packages/elements/src/message/index.ts
+++ packages/elements/src/message/index.ts
@@ -1,3 +1,21 @@
+export {
+  Action as MessageAction,
+  Actions as MessageActions,
+} from '../actions'
+
+export {
+  Branch as MessageBranch,
+  BranchMessages as MessageBranchMessages,
+  BranchNext as MessageBranchNext,
+  BranchPage as MessageBranchPage,
+  BranchPrevious as MessageBranchPrevious,
+  BranchSelector as MessageBranchSelector,
+} from '../branch'
+
+export {
+  Response as MessageResponse,
+} from '../response'
+
 export { default as Message } from './Message.vue'
 export { default as MessageAvatar } from './MessageAvatar.vue'
 export { default as MessageContent } from './MessageContent.vue'
