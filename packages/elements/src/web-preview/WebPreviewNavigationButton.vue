5b2e65abe44a6376410cfdc24389c309bd074d42
Quan <zhouquan147369@gmail.com>
2025-11-12 03:11:25 +0800

    refactor: update props definition to use TypeScript interface for improved type safety

--- packages/elements/src/web-preview/WebPreviewNavigationButton.vue
+++ packages/elements/src/web-preview/WebPreviewNavigationButton.vue
@@ -2,6 +2,7 @@
 import type { ButtonVariants } from '@repo/shadcn-vue/components/ui/button'
 import type { HTMLAttributes } from 'vue'
 import { Button } from '@repo/shadcn-vue/components/ui/button'
+
 import {
   Tooltip,
   TooltipContent,
@@ -9,12 +10,14 @@ import {
   TooltipTrigger,
 } from '@repo/shadcn-vue/components/ui/tooltip'
 
-const props = withDefaults(defineProps<HTMLAttributes & {
+interface Props extends /* @vue-ignore */ HTMLAttributes {
   size?: ButtonVariants['size']
   variant?: ButtonVariants['variant']
   tooltip?: string
   disabled?: boolean
-}>(), {
+}
+
+const props = withDefaults(defineProps<Props>(), {
   tooltip: '',
   disabled: false,
   size: 'sm',
