f1a820ee0ff80957b9983499ebc129b87f8076ef
snowbitx <WangXiaoYu_1995@outlook.com>
2025-11-25 14:42:51 +0800

    chore(registry): update Cloudflare preset and compatibility

--- apps/registry/nitro.config.ts
+++ apps/registry/nitro.config.ts
@@ -3,9 +3,9 @@ import { buildHooks } from './server/hooks'
 
 // https://nitro.build/config
 export default defineNitroConfig({
-  compatibilityDate: 'latest',
+  compatibilityDate: '2024-09-19',
   srcDir: 'server',
-  preset: 'cloudflare',
+  preset: 'cloudflare-module',
   hooks: buildHooks,
   serverAssets: [
     {
