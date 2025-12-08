88ea6057c972052677dce4fb7053b6dc6a8b9897
江上暮云 <aoveig10@163.com>
2025-12-08 15:09:56 +0800

    refactor: remove unnecessary useAttrs

--- packages/elements/src/message/MessageBranchContent.vue
+++ packages/elements/src/message/MessageBranchContent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@repo/shadcn-vue/lib/utils'
-import { computed, Fragment, isVNode, onMounted, useAttrs, useSlots, watch } from 'vue'
+import { computed, Fragment, isVNode, onMounted, useSlots, watch } from 'vue'
 import { useMessageBranchContext } from './context'
 
 interface Props {
@@ -9,7 +9,6 @@ interface Props {
 }
 
 const props = defineProps<Props>()
-const attrs = useAttrs()
 const slots = useSlots()
 
 const { currentBranch, setBranches } = useMessageBranchContext()
@@ -44,7 +43,7 @@ const baseClasses = computed(() => cn('grid gap-2 overflow-hidden [&>div]:pb-0',
   <template v-for="(node, index) in branchVNodes" :key="(node.key as any) ?? index">
     <div
       :class="cn(baseClasses, index === currentBranch ? 'block' : 'hidden')"
-      v-bind="attrs"
+      v-bind="$attrs"
     >
       <component :is="node" />
     </div>
