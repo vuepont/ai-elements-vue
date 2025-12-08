745135623a57122a2adc1ea83580db73c54d5add
Yuhuan <1219328322@qq.com>
2025-12-08 14:52:11 +0800

    chore: remove unnecessary useAttrs usage

--- packages/elements/src/message/MessageAvatar.vue
+++ packages/elements/src/message/MessageAvatar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 import { Avatar, AvatarFallback, AvatarImage } from '@repo/shadcn-vue/components/ui/avatar'
-import { computed, useAttrs } from 'vue'
+import { computed } from 'vue'
 
 interface Props {
   src: string
@@ -10,13 +10,12 @@ interface Props {
 }
 
 const props = defineProps<Props>()
-const attrs = useAttrs()
 
 const fallbackText = computed(() => props.name?.slice(0, 2) ?? 'ME')
 </script>
 
 <template>
-  <Avatar class="size-8 ring-1 ring-border" :class="[props.class]" v-bind="attrs">
+  <Avatar class="size-8 ring-1 ring-border" :class="[props.class]" v-bind="$attrs">
     <AvatarImage alt="" class="mt-0 mb-0" :src="props.src" />
     <AvatarFallback>{{ fallbackText }}</AvatarFallback>
   </Avatar>
