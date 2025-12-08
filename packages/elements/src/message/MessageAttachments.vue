d40d624cd57a203783a0079ea571eb8c5fcce504
Yuhuan <1219328322@qq.com>
2025-12-08 14:43:10 +0800

    chore: remove unnecessary useSlots usage

--- packages/elements/src/message/MessageAttachments.vue
+++ packages/elements/src/message/MessageAttachments.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@repo/shadcn-vue/lib/utils'
-import { useSlots } from 'vue'
 
 interface Props {
   class?: HTMLAttributes['class']
 }
 
 const props = defineProps<Props>()
-
-const slots = useSlots()
 </script>
 
 <template>
   <div
-    v-if="slots.default"
+    v-if="$slots?.default"
     :class="
       cn(
         'ml-auto flex w-fit flex-wrap items-start gap-2',
