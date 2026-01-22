f697e0b341668c8e8ea28951010b8f7b4c591428
peoray <peoray@yahoo.com>
2026-01-23 00:44:43 +0100

    refactor: simplify props definition in CommitFileIcon.vue

--- packages/elements/src/commit/CommitFileIcon.vue
+++ packages/elements/src/commit/CommitFileIcon.vue
@@ -3,13 +3,9 @@ import type { HTMLAttributes } from 'vue'
 import { cn } from '@repo/shadcn-vue/lib/utils'
 import { FileIcon } from 'lucide-vue-next'
 
-type CommitFileIconProps = InstanceType<typeof FileIcon>['$props']
-
-interface Props extends /* @vue-ignore */ CommitFileIconProps {
+const props = defineProps<{
   class?: HTMLAttributes['class']
-}
-
-const props = defineProps<Props>()
+}>()
 </script>
 
 <template>
