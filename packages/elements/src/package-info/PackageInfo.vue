1908758bdb0bcf2d44982a78cf336b47978bdb24
peoray <peoray@yahoo.com>
2026-01-23 17:57:34 +0100

    fix: add missing packages for the slot in packageinfo component

--- packages/elements/src/package-info/PackageInfo.vue
+++ packages/elements/src/package-info/PackageInfo.vue
@@ -4,6 +4,10 @@ import type { ChangeType } from './context'
 import { cn } from '@repo/shadcn-vue/lib/utils'
 import { provide } from 'vue'
 import { PackageInfoKey } from './context'
+import PackageInfoChangeType from './PackageInfoChangeType.vue'
+import PackageInfoHeader from './PackageInfoHeader.vue'
+import PackageInfoName from './PackageInfoName.vue'
+import PackageInfoVersion from './PackageInfoVersion.vue'
 
 interface Props extends /* @vue-ignore */ HTMLAttributes {
   name: string
@@ -28,6 +32,12 @@ provide(PackageInfoKey, {
     :class="cn('rounded-lg border bg-background p-4', props.class)"
     v-bind="$attrs"
   >
-    <slot />
+    <slot>
+      <PackageInfoHeader>
+        <PackageInfoName />
+        <PackageInfoChangeType v-if="changeType" />
+      </PackageInfoHeader>
+      <PackageInfoVersion v-if="currentVersion || newVersion" />
+    </slot>
   </div>
 </template>
