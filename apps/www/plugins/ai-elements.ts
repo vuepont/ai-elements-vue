6532d5536e58f3044120bfbcff107c11f392f268
cwandev <18888351756@163.com>
2026-01-27 15:44:55 +0800

    chore: remove explicit NuxtApp typing in ai-elements plugin

--- apps/www/plugins/ai-elements.ts
+++ apps/www/plugins/ai-elements.ts
@@ -1,10 +1,9 @@
-import type { NuxtApp } from 'nuxt/app'
 import * as Components from '@repo/examples'
 import ComponentLoader from '@/components/ComponentLoader.vue'
 import ComponentViewer from '@/components/ComponentViewer.vue'
 import ElementsDemo from '@/components/ElementsDemo.vue'
 
-export default defineNuxtPlugin((nuxtApp: NuxtApp) => {
+export default defineNuxtPlugin((nuxtApp) => {
   const { vueApp } = nuxtApp
 
   vueApp.component('ComponentLoader', ComponentLoader)
